49037b60fdd8d404a78c6f8a64299233849c6e1e
shhdyasser <shahdyasser108108@gmail.com>
2025-03-14 09:51:21 +0200

    initial commit

---
+++ src/main/java/testing/RecruitmentPage.java
@@ -0,0 +1,10 @@
+package testing;
+
+import org.openqa.selenium.WebDriver;
+
+public class RecruitmentPage extends PageBase{
+    public RecruitmentPage(WebDriver driver) {
+        super(driver);
+    }
+
+}
